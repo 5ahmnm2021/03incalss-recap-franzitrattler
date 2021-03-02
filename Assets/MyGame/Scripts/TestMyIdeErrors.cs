06ce2d2202f9980bb79d9fcb7fff25ae9ced0a2a
franzitrattler <franziska.trattler@gmail.com>
2021-03-02 09:08:07 +0100

    repository 03 with right folders from last week

---
+++ Assets/MyGame/Scripts/TestMyIdeErrors.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TestMyIdeErrors : MonoBehaviour
+{
+    string Tag = "TestMyIdeErrors";
+
+    int a = 1;
+    int b = 2;
+    float d = 5f;
+
+    TextMeshProUGUI TextPro;
+
+    void Start()
+    {
+        int c = a + b;
+        Debug.Log("a + b = " + 3);
+        Debug.Log(Tag + "- Done ;)");
+    }
+}
